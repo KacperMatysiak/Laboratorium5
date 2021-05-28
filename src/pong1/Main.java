8cd0088bbb06d7de53a81827d4bc88cee952eee0
KacperMatysiak <35163@studentwsei.pl>
2021-05-28 19:23:34 +0200

    Zadanie 4

--- src/pong1/Main.java
+++ src/pong1/Main.java
@@ -58,6 +58,24 @@ public class Pong1 extends Application {
     }
 
     private double x = ARENAX1+ARENAWIDTH/2;
+    private double y = ARENAY1+ARENAHEIGHT/2;
+
+    private double vx = 4;
+    private double vy = 4;
+
+    private void run(GraphicsContext gc) {
+        gc.setFill(Color.Black);
+        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
+
+        if ((x <= ARENAX1) || ((x >= ARENAX2))) vx = -vx;
+        if ((x <= ARENAY1) || ((x >= ARENAY2))) vy = -vy;
+
+        x += vx;
+        y += vy;
+
+        gc.setFill(Color.WHITESMOKE);
+        gc.fillOval(x,y,2*R,2*R);
+    }
 
     private void run(GraphicsContext gc) {
         gc.setFill(Color.BLACK);
