bc69ce866150cc0f83d2b708c7d23c4a84a9137b
KacperMatysiak <35163@studentwsei.pl>
2021-05-28 19:16:47 +0200

    3 zadanie zrobione

--- src/pong1/Main.java
+++ src/pong1/Main.java
@@ -7,6 +7,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.time.Duration;
+import java.util.EventListener;
 
 public class Pong1 extends Application {
 
@@ -25,16 +28,40 @@ public class Pong1 extends Application {
     public void start(Stage stage) {
         Canvas canvas = new Canvas(WIDTH, HEIGHT);
         GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
+        t.setCycleCount(Timeline.INDEFINITE);
+
         stage.setTitle("Kulki!");
         stage.setScene(new Scene(new StackPane(canvas)));
         stage.show();
 
+        /*
         gc.setFill(Color.BLACK);
         gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAWIDTH);/
         gc.setFill(Color.WHITESMOKE);
         gc.fillOval(ARENAX1+ARENAWIDTH/2,ARENAY1+ARENAHEIGHT/2,2*R, 2*R);
+        */
+
+        t.play();
+    }
+    public void KeyFrame(@NamedArg("time") Duration time, @NamedArg("onFinished") EventHandler<ActionEvent> onFinished){
+
+    }
+    @FunctionalInterface
+    public interface EventHandler<T extends Event> extends EventListener {
+        void handle(T event);
     }
 
+    private double x = ARENAX1+ARENAWIDTH/2;
+
+    private void run(GraphicsContext gc) {
+        gc.setFill(Color.BLACK);
+        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
+
+        gc.setFill(Color.WHITESMOKE);
+        gc.fillOval(x += 5,ARENAY1+ARENAHEIGHT/2, 2*R, 2*R);
+    }
 
     public static void main(String[] args) {
         launch(args);
