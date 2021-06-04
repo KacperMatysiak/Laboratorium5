eb477ea586c27ccb24af34023b2ed48e101f93ba
KacperMatysiak <35163@studentwsei.pl>
2021-06-04 11:30:42 +0200

    Zadanie 5

--- src/pong1/Pong1.java
+++ src/pong1/Pong1.java
@@ -1,6 +1,5 @@
 package pong1;
 
-
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
@@ -14,13 +13,12 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
-
 import java.util.EventListener;
 
 public class Pong1 extends Application {
 
     private static final double WIDTH =800;
-    private static final double HEIGHT = 600;
+    private static final double HEIGHT = 700;
     private static final double MARGIN = 50;
     private static final double ARENAWIDTH = WIDTH - 2*MARGIN;
     private static final double ARENAHEIGHT = HEIGHT - 2*MARGIN;
@@ -29,54 +27,46 @@ public class Pong1 extends Application {
     private static final double ARENAX2 = ARENAX1 + ARENAWIDTH;
     private static final double ARENAY2 = ARENAY1 + ARENAHEIGHT;
     private static final double R = 10;
+    private double x = ARENAX1+ARENAWIDTH/2;
+    private double y = ARENAY1+ARENAHEIGHT/2;
+    private double vx = 4;
+    private double vy = 4;
 
-    @Override
-    public void start(Stage stage) {
-        Canvas canvas = new Canvas(WIDTH, HEIGHT);
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-
-        Timeline t = new Timeline(new KeyFrame(Duration.millis(100), e -> run(gc)));
-        t.setCycleCount(Timeline.INDEFINITE);
-
-        stage.setTitle("Kulki!");
-        stage.setScene(new Scene(new StackPane(canvas)));
-        stage.show();
-
-        /*
-        gc.setFill(Color.BLACK);
-        gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAWIDTH);/
-        gc.setFill(Color.WHITESMOKE);
-        gc.fillOval(ARENAX1+ARENAWIDTH/2,ARENAY1+ARENAHEIGHT/2,2*R, 2*R);
-        */
-
-        t.play();
-    }
     public void KeyFrame(@NamedArg("time") Duration time, @NamedArg("onFinished") EventHandler<ActionEvent> onFinished){
 
     }
+
     @FunctionalInterface
     public interface EventHandler<T extends Event> extends EventListener {
         void handle(T event);
     }
 
-    private double x = ARENAX1+ARENAWIDTH/2;
-    private double y = ARENAY1+ARENAHEIGHT/2;
-
-    private double vx = 4;
-    private double vy = 4;
-
     private void run(GraphicsContext gc) {
         gc.setFill(Color.BLACK);
         gc.fillRect(ARENAX1, ARENAY1, ARENAWIDTH, ARENAHEIGHT);
 
-        if ((x <= ARENAX1) || ((x >= ARENAX2))) vx = -vx;
-        if ((x <= ARENAY1) || ((x >= ARENAY2))) vy = -vy;
+        if ((x <= ARENAX1) || ((x >= ARENAX2-20))) vx = -vx;
+        if ((y <= ARENAY1) || ((y >= ARENAY2-20))) vy = -vy;
 
         x += vx;
         y += vy;
 
         gc.setFill(Color.WHITESMOKE);
         gc.fillOval(x,y,2*R,2*R);
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Canvas canvas = new Canvas(WIDTH, HEIGHT);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Timeline t = new Timeline(new KeyFrame(Duration.millis(30), e -> run(gc)));
+        t.setCycleCount(Timeline.INDEFINITE);
+
+        stage.setTitle("Kulki!");
+        stage.setScene(new Scene(new StackPane(canvas)));
+        stage.show();
+        t.play();
     }
 
     public static void main(String[] args) {
